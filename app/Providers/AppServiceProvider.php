d2966c7957f8462c3771b4e12a3fcf313a416f6e
Shahnawaz-dt <coreteam.dt@gmail.com>
2026-02-14 12:25:51 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,17 +13,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-    public function boot(): void
-{
-    if ($this->app->environment('production')) {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-    }
+
 }
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+        Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
